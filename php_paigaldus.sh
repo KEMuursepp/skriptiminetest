67c4f32a66954513fad100610bd841f5384a2286
KEMuursepp <karl-edward.muursepp@voco.ee>
2025-01-23 11:29:44 +0200

    php paigaldamine

---
+++ php_paigaldus.sh
@@ -0,0 +1,10 @@
+PHP=$(dpkg-query -W -f='${Status}' php8.1 2>/dev/null | grep -c "ok installed" )
+
+if [ $PHP -eq 0 ]; then
+    echo "PHP ja lisad paigaldatud"
+    apt install php8.1 libapache2-mod-php8.1 php8.1-mysql
+    echo "php installed"
+elif [ $PHP -eq 1 ]; then
+    echo "php perma installed"
+    which php
+fi
