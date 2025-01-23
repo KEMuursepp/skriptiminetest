1e0e4e9e300146c00da98a95614b31a15d2e0004
Karl-Edward Müürsepp <karl-edward.muursepp@voco.ee>
2025-01-23 11:39:00 +0200

    php_paigaldus.sh

--- php_paigaldus.sh
+++ php_paigaldus.sh
@@ -1,10 +1,11 @@
-PHP=$ (dphg-query -W -f='${Status} php7.0 2>/dev/null | grep -c 'ok installed')
+PHP=$(dpkg-query -W -f='${Status}' php8.1 2>/dev/null | grep -c "ok installed" )
 
 if [ $PHP -eq 0 ]; then
     echo "PHP ja lisad paigaldatud"
-    apt install php7.0 libapache2-mod-php7.0 php7.0-mysql
+    apt install php8.1 libapache2-mod-php8.1 php8.1-mysql
     echo "php installed"
 elif [ $PHP -eq 1 ]; then
     echo "php perma installed"
     which php
 fi
+
